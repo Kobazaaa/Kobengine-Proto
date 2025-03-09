440008a2b14bd493128b10d4d6c6767f25bc9353
Kobe Dereyne <kobe.dereyne@outlook.com>
2025-03-09 12:26:46 +0100

    Normalize Direction in MoveCommand

--- Kobengine/Commands/MoveCommands.cpp
+++ Kobengine/Commands/MoveCommands.cpp
@@ -3,9 +3,12 @@
 
 kob::MoveCommand::MoveCommand(GameObject& gameobject, const glm::vec3& dir, float speed)
 	: m_pGameObject{ &gameobject }
-	, m_Direction{ dir }
 	, m_Speed{ speed }
 {
+	// Normalize Direction
+	const float magnitude = std::sqrt(dir.x * dir.x + dir.y * dir.y + dir.z * dir.z);
+	if (abs(magnitude) <= FLT_EPSILON) m_Direction = glm::vec3();
+	else m_Direction = dir / magnitude;
 }
 
 void kob::MoveCommand::Execute()
