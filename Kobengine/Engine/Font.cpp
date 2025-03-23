c4c42c5c1a589cb59eb0f0efca7c1382343a8db3
Kobe Dereyne <kobe.dereyne@outlook.com>
2025-03-23 10:37:51 +0100

    Restructure Font class

--- Kobengine/Engine/Font.cpp
+++ Kobengine/Engine/Font.cpp
@@ -2,20 +2,29 @@
 #include <SDL_ttf.h>
 #include "Font.h"
 
-TTF_Font* kob::Font::GetFont() const {
-	return m_font;
-}
 
-kob::Font::Font(const std::string& fullPath, unsigned int size) : m_font(nullptr)
+//--------------------------------------------------
+//    Constructor & Destructor
+//--------------------------------------------------
+kob::Font::Font(const std::string& fullPath, unsigned int size)
+	: m_pFont(nullptr)
 {
-	m_font = TTF_OpenFont(fullPath.c_str(), size);
-	if (m_font == nullptr) 
+	m_pFont = TTF_OpenFont(fullPath.c_str(), size);
+	if (m_pFont == nullptr)
 	{
 		throw std::runtime_error(std::string("Failed to load font: ") + SDL_GetError());
 	}
 }
-
 kob::Font::~Font()
 {
-	TTF_CloseFont(m_font);
+	TTF_CloseFont(m_pFont);
+}
+
+
+//--------------------------------------------------
+//    Accessors & Mutators
+//--------------------------------------------------
+TTF_Font* kob::Font::GetFont() const
+{
+	return m_pFont;
 }
--- Kobengine/Engine/Font.h
+++ Kobengine/Engine/Font.h
@@ -4,21 +4,29 @@
 struct _TTF_Font;
 namespace kob
 {
-	/**
-	 * Simple RAII wrapper for a _TTF_Font
-	 */
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// ~~	  Simple RAII wrapper for a _TTF_Font
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	class Font final
 	{
 	public:
-		_TTF_Font* GetFont() const;
+		//--------------------------------------------------
+		//    Constructor & Destructor
+		//--------------------------------------------------
 		explicit Font(const std::string& fullPath, unsigned int size);
 		~Font();
 
-		Font(const Font &) = delete;
-		Font(Font &&) = delete;
-		Font & operator= (const Font &) = delete;
-		Font & operator= (const Font &&) = delete;
+		Font(const Font&) = delete;
+		Font(Font&&) = delete;
+		Font& operator= (const Font&) = delete;
+		Font& operator= (const Font&&) = delete;
+
+		//--------------------------------------------------
+		//    Accessors & Mutators
+		//--------------------------------------------------
+		_TTF_Font* GetFont() const;
+
 	private:
-		_TTF_Font* m_font;
+		_TTF_Font* m_pFont;
 	};
 }
